5a4355fb457b18e86ef6e5f880b139f91aa60d8a
eraltafs <43698833+eraltafs@users.noreply.github.com>
2022-12-14 00:10:22 +0530

    footer done

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -1,5 +1,8 @@
 import navbar  from "../components/navbar.js";
+import footer from "../components/footer.js";
 document.getElementById("navigations").innerHTML = navbar()
+
+document.getElementById("footer").innerHTML = footer()
 
 document.getElementById("logo").onclick = ()=>{
     location.href = "/"
