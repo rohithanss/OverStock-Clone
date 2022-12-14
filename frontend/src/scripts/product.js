85437681a0074d391d71bd6978b55fb5a1c4772b
riyaa-1099 <112823546+riyaa-1099@users.noreply.github.com>
2022-12-14 18:50:50 +0530

    productpage

---
+++ frontend/src/scripts/product.js
@@ -0,0 +1,49 @@
+import navbar  from "../components/navbar.js";
+import footer from "../components/footer.js";
+document.getElementById("navigations").innerHTML = navbar()
+
+document.getElementById("footer").innerHTML = footer()
+
+document.getElementById("logo").onclick = ()=>{
+    location.href = "/"
+}
+
+
+const getProducts=async()=>{
+try{
+let result=await fetch(`http://localhost:7010/products?category=refrigerator`)
+
+console.log(result)
+let data= await result.json()
+appendProducts(data.products)
+}
+catch(err){
+console.log(err)
+console.log("Something went wrong")
+}
+}
+getProducts()
+const appendProducts=(data)=>{
+   let data_div=document.getElementById("betright1")
+
+    data.forEach((el)=>{
+       let div=document.createElement("div");
+       div.setAttribute("class","singleProduct")
+        let image=document.createElement("img");
+        image.class="imagepro";
+        image.src=el.image;
+       image.style.width="340px";
+        let price=document.createElement("b");
+        price.innerHTML=`Sale Starts at INR ${el.price}`
+
+        let title=document.createElement("p");
+        title.innerHTML=el.title;
+       div.append(image,price,title)
+       data_div.append(div)
+     })
+
+}
+
+
+
+
